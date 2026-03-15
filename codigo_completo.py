443a391c782d2429b1fea81d372e725731f18d5c
weeesh23w <ykz@optimizer.com>
2026-03-15 05:13:01 -0400

    YKZ v2.8.3: Testing update mechanism

--- codigo_completo.py
+++ codigo_completo.py
@@ -18,7 +18,7 @@ import subprocess
 from PIL import Image, ImageTk
 
 # --- Configuration & Theme ---
-CURRENT_VERSION = "2.8.2"
+CURRENT_VERSION = "2.8.3"
 # [USER CONFIG] Cambia esto por la URL RAW de tu archivo version.json en GitHub/Pastebin
 # Ejemplo estructura JSON: {"version": "2.1.0", "url": "https://link/to/new_exe.exe"}
 UPDATE_JSON_URL = "https://raw.githubusercontent.com/weeesh23w/ykz-opti/main/version.json" 
